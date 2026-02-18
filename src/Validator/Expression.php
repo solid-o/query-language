0ced46fb14308fcda300b531ed8d0232e349d44c
Alessandro Chitolina <alekitto@gmail.com>
2026-02-18 01:48:17 +0100

    fix: update expression constraint not to use options array

--- src/Validator/Expression.php
+++ src/Validator/Expression.php
@@ -9,23 +9,16 @@ use Solido\QueryLanguage\Walker\Validation\ValidationWalkerInterface;
 use Symfony\Component\Validator\Constraint;
 use TypeError;
 
-use function array_merge;
 use function get_debug_type;
-use function is_array;
 use function is_callable;
 use function is_string;
 use function sprintf;
 
-/**
- * @Annotation
- * @Target({"PROPERTY"})
- */
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class Expression extends Constraint
 {
     /**
-     * @var string|callable|ValidationWalkerInterface|null
-     * @Required()
+     * @var string|callable|ValidationWalkerInterface
      */
     public $walker;
 
@@ -34,17 +27,13 @@ class Expression extends Constraint
      *
      * @param mixed $walker
      */
-    public function __construct(mixed $walker, array|null $groups = null, $payload = null, array $options = [])
+    public function __construct(mixed $walker, array|null $groups = null, $payload = null)
     {
-        if (is_array($walker)) {
-            $options = array_merge($walker, $options);
-        } elseif ($walker !== null && ! is_string($walker) && ! is_callable($walker) && ! $walker instanceof ValidationWalkerInterface) {
+        if (! is_string($walker) && ! is_callable($walker) && ! $walker instanceof ValidationWalkerInterface) {
             throw new TypeError(sprintf('"%s": Expected argument $walker to be either a string, a callable, an instance of %s or an array, got "%s".', __METHOD__, ValidationWalkerInterface::class, get_debug_type($walker)));
-        } else {
-            $options['walker'] = $walker;
         }
 
-        parent::__construct($options, $groups, $payload);
+        parent::__construct(null, $groups, $payload);
     }
 
     public function getDefaultOption(): string
